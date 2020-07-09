652df50bdbe765bf6d129ec80af3f42ca6332511
jinchunzhao <459147801@qq.com>
2020-07-09 09:43:11 +0800

    Initial commit

---
+++ src/main/java/com/github/jinchunzhao/trimspace/interceptor/TrimSpaceInterceptor.java
@@ -0,0 +1,87 @@
+
+package com.github.jinchunzhao.trimspace.interceptor;
+
+
+import com.github.jinchunzhao.trimspace.util.TrimUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+
+/**
+ * 自定义拦截器--实现去除首尾空格处理
+ *
+ * @author JinChunZhao
+ * @version 1.0
+ * @date 2020-06-23 19:50
+ */
+@Component
+@Aspect
+public class TrimSpaceInterceptor implements HandlerInterceptor {
+
+    /**
+     * 环绕处理
+     *
+     * @param proceedingJoinPoint 连接点
+     * @return 结果
+     * @throws Throwable 异常
+     */
+    @Around(value = "@annotation(com.github.jinchunzhao.trimspace.annotation.TrimSpace)")
+    public Object trimAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+//        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+//        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+//        HttpServletRequest request = sra.getRequest();
+
+//        String url = request.getRequestURL().toString();
+//        String method = request.getMethod();
+//        String uri = request.getRequestURI();
+//        String queryString = request.getQueryString();
+//        log.info("使用注解去除controller入参前后空格的请求来了url:【{}】,方法:【[]】,uri:【{}】,queryString:【{}】", url, method, uri, queryString);
+
+        // 获取连接点的入参
+        Object[] objects = proceedingJoinPoint.getArgs();
+        if (Objects.nonNull(objects) && objects.length > 0) {
+            // 处理接口传参：去除首尾空格
+            for (int i = 0, len = objects.length; i < len; i++) {
+                objects[i] = TrimUtils.trimExe(objects[i]);
+            }
+        }
+        // 放行
+        return proceedingJoinPoint.proceed(objects);
+    }
+
+
+
+    /**
+     * 后置处理
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3) throws Exception {
+
+    }
+
+    /**
+     * 后处理回调方法
+     */
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3) throws Exception {
+
+    }
+
+    /**
+     * 预处理回调方法：在请求处理之前进行调用
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
+        //不需要前置处理
+        return false;
+    }
+
+}
---
+++ src/main/java/com/github/jinchunzhao/trimspace/util/TrimUtils.java
@@ -0,0 +1,261 @@
+package com.github.jinchunzhao.trimspace.util;
+
+
+import org.springframework.util.CollectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 去除字符串前后空格工具类
+ *
+ * @author JinChunZhao
+ * @version 1.0
+ * @date 2020-07-02 22:57
+ */
+
+public class TrimUtils {
+
+    /**
+     * 非字符串类型的原始数据类型
+     */
+    private static final List<Class<?>> WRAPPER = Arrays.asList(Byte.class, Short.class,
+            Integer.class, Long.class, Float.class, Double.class, Boolean.class);
+
+    /**
+     * 符串类型的原始数据类型
+     */
+    private static final List<Class<?>> STRING_PER = Arrays.asList(Character.class, String.class);
+
+    /**
+     * 处理所有类型参数:基本数据类型、实体对象、List、数组、set、map，去除字符串首尾空格
+     *
+     * @param object 对象
+     * @return 结果
+     * @throws IllegalAccessException 异常
+     */
+    public static Object trimExe(Object object) throws IllegalAccessException {
+        if (Objects.isNull(object)) {
+            return object;
+        }
+        Class<? extends Object> clazz = object.getClass();
+        // 为非String Char包装类型，不做处理
+        if (WRAPPER.contains(clazz)) {
+            return object;
+        }
+        if (STRING_PER.contains(clazz)) {
+            return object.toString().trim();
+        }
+        // 字段为private时
+        Field[] fields = clazz.getDeclaredFields();
+        if (Objects.isNull(fields) || fields.length == 0) {
+            return object;
+        }
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object filedValue = field.get(object);
+            if (Objects.nonNull(filedValue)) {
+                Class<?> aClass = filedValue.getClass();
+                if (filedValue instanceof Collection) {
+                    trimList(object, field);
+                } else if (aClass.isArray()) {
+                    trimArray(object, field); //数组中含对象时有bug
+                } else if (filedValue instanceof Map) {
+                    trimMap(object, field);
+                } else if (filedValue instanceof String || filedValue instanceof Character) {
+                    trimStringFiled(object, field);
+                } else if (WRAPPER.contains(aClass)) {
+                    continue;
+                } else {
+                    trimObjectFiled(object, field);
+                }
+            }
+        }
+
+        return object;
+    }
+
+    /**
+     * 处理字符串类型的数据
+     *
+     * @param object
+     * @param field
+     * @return
+     * @throws IllegalAccessException
+     */
+    public static Object trimStringFiled(Object object, Field field) throws IllegalAccessException {
+        Class<?> filedType = field.getType();
+        if (filedType == String.class || filedType == Character.class) {
+            field.setAccessible(true);
+            if (Objects.nonNull(field.get(object))) {
+                // 在原有的对象上设置去除首尾空格的新值
+                field.set(object, String.valueOf(field.get(object)).trim());
+            }
+        }
+        return object;
+    }
+
+    /**
+     * 处理实体对象类型的数据
+     *
+     * @param object
+     * @param field1
+     * @return
+     * @throws IllegalAccessException
+     */
+    public static Object trimObjectFiled(Object object, Field field1) throws IllegalAccessException {
+        Object filedValue1 = field1.get(object);
+        Class<?> filedType1 = field1.getType();
+        if (Objects.isNull(field1)) {
+            return field1;
+        }
+        // 为非String Char包装类型，不做处理
+        if (WRAPPER.contains(filedType1)) {
+            return object;
+        }
+        if (filedType1 == String.class || filedType1 == Character.class) {
+            return filedValue1.toString().trim();
+        }
+        // 字段为private时，无法修改字段值，需要反射
+        Field[] fields = filedType1.getDeclaredFields();
+        if (Objects.isNull(fields) || fields.length == 0) {
+            return object;
+        }
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object filedValue = field.get(filedValue1);
+            if (Objects.nonNull(filedValue)) {
+                Class<?> aClass = filedValue.getClass();
+                if (filedValue instanceof List) {
+                    trimList(filedValue1, field);
+                } else if (aClass.isArray()) { //判断这个对象是否是数组
+                    trimArray(filedValue1, field);
+                } else if (filedValue instanceof Map) {
+                    trimMap(filedValue1, field);
+                } else if (filedValue instanceof String || filedValue instanceof Character) {
+                    trimStringFiled(filedValue1, field);
+                } else if (WRAPPER.contains(aClass)) {
+                    continue;
+                } else {
+                    trimObjectFiled(filedValue1, field);
+                }
+            }
+            Class<?> filedType = field.getType();
+            // 只处理字符串类型
+            if (filedType != String.class) {
+                continue;
+            }
+            if (filedType == String.class || filedType == Character.class) {
+                // 去除private权限，变为可更改
+                field.setAccessible(true);
+                if (Objects.nonNull(field.get(filedValue1))) {
+                    // 在原有的对象上设置去除首尾空格的新值
+                    field.set(filedValue1, String.valueOf(field.get(filedValue1)).trim());
+                }
+            }
+        }
+        return object;
+    }
+
+    /**
+     * 处理Collection类型的数据
+     *
+     * @param object
+     * @param field
+     * @return
+     * @throws IllegalAccessException
+     */
+    public static Object trimList(Object object, Field field) throws IllegalAccessException {
+        Object filedValue = field.get(object);
+        Object[] objs = ((Collection) filedValue).toArray();
+        if (Objects.isNull(objs)) {
+            return object;
+        }
+        if (objs.length == 0) {
+            return object;
+        }
+        List<String> list = new ArrayList<>();
+        for (int index = 0; index < objs.length; index++) {
+            Object e1 = objs[index];
+            if (e1.getClass() == String.class) {
+                String trim = String.valueOf(e1).trim();
+                list.add(trim);
+            } else {
+                trimExe(e1);
+            }
+        }
+        if (Objects.nonNull(list) && list.size() > 0) {
+            field.set(object, list);
+        }
+        return object;
+    }
+
+    /**
+     * 处理MAP类型的数据
+     *
+     * @param object
+     * @param field
+     * @return
+     * @throws IllegalAccessException
+     */
+    public static Object trimMap(Object object, Field field) throws IllegalAccessException {
+        Object filedValue = field.get(object);
+        Map<Object, Object> filedValueMap = (Map<Object, Object>) filedValue;
+        if (CollectionUtils.isEmpty(filedValueMap)) {
+            return object;
+        }
+        for (Map.Entry<Object, Object> entry : filedValueMap.entrySet()) {
+            Object entryValue = entry.getValue();
+            Class<?> aClass1 = entryValue.getClass();
+            if (aClass1 == String.class) {
+                String trim = String.valueOf(entryValue).trim();
+                entry.setValue(trim);
+            } else {
+                trimExe(entryValue);
+            }
+        }
+        if (!CollectionUtils.isEmpty(filedValueMap)) {
+            field.set(object, filedValueMap);
+        }
+        return object;
+    }
+
+    /**
+     * 处理数组类型的数据
+     *
+     * @param object
+     * @param field
+     * @return
+     * @throws IllegalAccessException
+     */
+    public static Object trimArray(Object object, Field field) throws IllegalAccessException {
+        Object filedValue = field.get(object);
+        Object[] objs = (Object[]) filedValue;
+        if ( Objects.isNull(objs)) {
+            return object;
+        }
+        if (objs.length == 0) {
+            return object;
+        }
+        List<String> list = new ArrayList<>();
+        for (int index = 0; index < objs.length; index++) {
+            Object e1 = objs[index];
+            if (e1.getClass() == String.class) {
+                String trim = String.valueOf(e1).trim();
+                list.add(trim);
+                objs[index] = trim;
+            } else {
+                trimExe(e1);
+            }
+        }
+        if (Objects.nonNull(objs) && objs.length > 0) {
+            field.set(object, objs);
+        }
+        return object;
+    }
+}
